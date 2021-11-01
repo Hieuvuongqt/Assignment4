ed781b6b5063546ea6918d598ffa362b2ee14eb9
hieuvd1 <72237853+Hieuvuongqt@users.noreply.github.com>
2021-11-01 09:35:42 +0700

    Update Assignment4_Exercise3.cpp

--- Assignment4_Exercise3.cpp
+++ Assignment4_Exercise3.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 int main(){
 	int a,b,c;
+	float p,s=0,P=0;
 	printf("Enter the first number: ");
 	scanf("%d",&a);
 	printf("Enter the second number: ");
@@ -9,4 +10,22 @@ int main(){
 	printf("Enter the third number: ");
 	scanf("%d",&c);
 	
-	if
+	 if((a*a<b*b+c*c) && (b*b<a*a+c*c) && (c*c<b*b+a*a)) {
+
+                   printf("\nThese are the three sides of a triangle");
+
+                   p = (a+b+c)/2;
+
+                   s = sqrt(p*(p-a)*(p-b)*(p-c));
+
+                   printf("\nThe area of the triangle is %f", s);
+		  
+                   P = a+b+c;
+                   
+		   printf("\nThe perimeter of the triangle is %f", P);
+
+          } else
+
+                   printf("\nThese are not the three sides of a triangle");
+
+}
